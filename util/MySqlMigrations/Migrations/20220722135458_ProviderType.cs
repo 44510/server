0f2478f3c402f6b440f066180cbe89dcb3b45bf1
Rui Tome <rtome@bitwarden.com>
2022-07-26 10:43:01 +0100

    Added MySql migrations and script

---
+++ util/MySqlMigrations/Migrations/20220722135458_ProviderType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.MySqlMigrations.Migrations
+{
+    public partial class ProviderType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "ProviderType",
+                table: "Provider",
+                type: "tinyint unsigned",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProviderType",
+                table: "Provider");
+        }
+    }
+}
