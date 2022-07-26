a14c0daee193f03df7944558af451624a92d3f86
Rui Tome <rtome@bitwarden.com>
2022-07-26 10:43:09 +0100

    Added Postgres migrations and script

---
+++ util/PostgresMigrations/Migrations/20220726085350_ProviderType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    public partial class ProviderType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "ProviderType",
+                table: "Provider",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProviderType",
+                table: "Provider");
+        }
+    }
+}
---
+++ util/PostgresMigrations/Scripts/2022-07-22_00_ProviderType.psql
@@ -0,0 +1,8 @@
+START TRANSACTION;
+
+ALTER TABLE `Provider` ADD `ProviderType` tinyint unsigned NOT NULL DEFAULT 0;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20220722135458_ProviderType', '6.0.4');
+
+COMMIT;
